5372ed4d181915ceeba72bd1ab5bc13b741d41c7
t-nician <dev@senpai.zone>
2024-06-21 10:39:23 -0400

    quick push.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from dataclasses import replace
 from mcon import from_config
 from mcon.watchdog import Watchdog, Command
 
-from mcon.quirks.mordhau import MordhauSession, MordhauPlayer
+from mcon.quirks.mordhau import MordhauSession, MordhauPlayer, EventType
 
 watchdog: Watchdog = from_config("./config.jsonc")
 mordhau: MordhauSession = MordhauSession(watchdog=watchdog)
@@ -15,5 +15,15 @@ mordhau: MordhauSession = MordhauSession(watchdog=watchdog)
 def playtime_command(player):
     print(player)
 
+
+@mordhau.on_event(EventType.PLAYER_JOIN)
+def player_joined(player: MordhauPlayer):
+    print(player.id, player.name, "has joined!")
+
+
+@mordhau.on_event(EventType.PLAYER_LEAVE)
+def player_leave(player: MordhauPlayer):
+    print(player.id, player.name, "has left!")
+    
 
 mordhau.start()
--- src/mcon/quirks/mordhau.py
+++ src/mcon/quirks/mordhau.py
@@ -8,7 +8,10 @@ from mcon.watchdog import Command
 
 from mcon.quirks.playfab import PlayfabPlayer
 
-
+class EventType(Enum):
+    PLAYER_LEAVE = "player_left"
+    PLAYER_JOIN = "player_join"
+    
 
 class PlayerType(Enum):
     ADMIN = "admin"
@@ -31,21 +34,71 @@ class Chatlog(PlayfabPlayer):
     timestamp: int = field(default=0)
 
 
+previous_playerlist: list[MordhauPlayer] = []
+
 @dataclass
 class MordhauSession(Session):
     playerlist: list[MordhauPlayer] = field(default_factory=list)
     chatlogs: list[Chatlog] = field(default_factory=list)
     
-    chat_commands: dict[str, (tuple[MordhauPlayer, str])] = field(default_factory=list)
+    chat_commands: dict[str, tuple[(tuple[MordhauPlayer, str]), int]] = field(default_factory=dict)
+    event_listeners: list[tuple[EventType, (MordhauPlayer)]] = field(default_factory=list)
     
     def chat_command(self, command: str, args: int = 0):
         def wrapper(callback: (tuple[MordhauPlayer, str])):
-            print(callback)
+            if self.chat_commands.get(command) is None:
+                self.chat_commands[command] = (callback, args)
+        return wrapper
+    
+    
+    def on_event(self, event_type: EventType):
+        def wrapper(callback: (MordhauPlayer)):
+            self.event_listeners.append((event_type, callback))
         return wrapper
     
     
     def prestart(self):
-        @self.watchdog.command(Command("playerlist"), interval_seconds=1)
+        @self.watchdog.command(Command("playerlist"), interval_seconds=5)
         def playerlist(command: Command):
+            global previous_playerlist
+            
             print(command.result)
+            
+            if command.result == "There are currently no players present":
+                previous_playerlist = []
+                return None
+            
+            player_str_list = command.result.split("\n")
+            player_str_list.pop()
+            
+            current_playerlist = []
+            
+            for player_str in player_str_list:
+                playfab, name, ping, team = player_str.split(", ")
+
+                current_playerlist.append(
+                    MordhauPlayer(
+                        name,
+                        playfab,
+                        team,
+                        ping,
+                        PlayerType.PLAYER
+                    )
+                )
+            
+            
+            for player in current_playerlist:
+                if previous_playerlist.count(player) == 0:
+                    self.__fire_event(EventType.PLAYER_JOIN, player)
+            
+            for player in previous_playerlist:
+                if current_playerlist.count(player) == 0:
+                    self.__fire_event(EventType.PLAYER_LEAVE, player)
+            
+            previous_playerlist = current_playerlist
+            
     
+    def __fire_event(self, event_type: EventType, *args):
+        for event_listener in self.event_listeners:
+            if event_listener[0] == event_type:
+                event_listener[1](*args)
