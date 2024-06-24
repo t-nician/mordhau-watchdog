ec4341bc938e87f32556f73afb08e28a72320e8b
t-nician <dev@senpai.zone>
2024-06-23 23:28:28 -0400

    epic push

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import time
+
 from mcon import from_config
 
 from mcon.watchdog import Watchdog, BroadcastType
@@ -13,14 +15,22 @@ def chat(mordhau_player: MordhauPlayer, channel, message):
     print(f"{mordhau_player.id} {mordhau_player.name} said '{message}'")
 
 
+player_timestamps: dict[str, int] = {}
 @mordhau.watchdog.on_broadcast(EventType.PLAYER_PRESENCE)
 def presence(mordhau_player: MordhauPlayer, is_joining):
+    global player_timestamps
+    
     player_str = f"{mordhau_player.id} {mordhau_player.name} "
     
     if is_joining:
         player_str = player_str + "is joining!"
+        player_timestamps[mordhau_player.id] = time.time()
     else:
-        player_str = player_str + "is leaving!"
+        playtime = time.time() - player_timestamps[mordhau_player.id]
+        
+        del player_timestamps[mordhau_player.id]
+        
+        player_str = player_str + f"is leaving!\nPlayed for {playtime} seconds..."
     
     print(player_str)
 
